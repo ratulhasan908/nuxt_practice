62a88f9c966c63e11b9efeff84d97e3068a68d91
Ratul Hasan <ratulhasan.nu@gmail.com>
2024-02-07 10:40:55 +0600

    New formatted file

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,32 @@
 <script>
-  export default {
-    data() {
-      return {
-        posts: []
-      };
-    },
-    created() {
-      fetch('https://jsonplaceholder.typicode.com/photos')
-        .then((res) => res.json())
-        .then((posts) => {
-          this.posts = posts;
-        });
-    },
+export default {
+  data() {
+    return {
+      posts: []
+    };
+  },
+  created() {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+      .then((res) => res.json())
+      .then((posts) => {
+        this.posts = posts;
+      });
+  },
 
 
-computed: {
-  posts: function () {
-     return this.posts.slice(0, 10)
-     }
+  computed: {
+    posts: function () {
+      return this.posts.slice(0, 10)
+    }
   }
-  };
+};
 </script>
 
 <template>
   <ul>
     <li v-for="post in posts" :key="post.id">
-        <div>{{ post.id }}</div>
-        <div><img :src="post.url" alt=""></div>
+      <div>{{ post.id }}</div>
+      <div><img :src="post.url" alt=""></div>
     </li>
   </ul>
 </template>
